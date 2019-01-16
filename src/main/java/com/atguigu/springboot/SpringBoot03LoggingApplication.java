3a84c8767a331b1c2e18a3382d9fc1045f7aba08
zeke_glb <zg_zeke@126.com>
2019-01-16 18:41:43 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/springboot/SpringBoot03LoggingApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot03LoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot03LoggingApplication.class, args);
+    }
+
+}
+
